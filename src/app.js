6149af1a5dcf764bf5e18f01b80697665d5b51ac
timo131 <thomasemosley3@gmail.com>
2025-03-16 20:02:51 +0000

    bootstrap added

--- src/app.js
+++ src/app.js
@@ -2,47 +2,70 @@ import "bootstrap";
 import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+
 window.onload = function () {
-  console.log("Hello Rigo from the console!");
-  const pronoun = ['the', 'our'];
-  const adj = ['great', 'big'];
-  const noun = ['jogger', 'racoon', 'paredes', 'partio'];
-  const dominio = ['.com', '.es', '.io', '.net'];
-  let container = document.getElementById("resultados");
+  const pronouns = ['the', 'our'];
+  const adjectives = ['great', 'big'];
+  const nouns = ['jogger', 'racoon', 'paredes', 'partio'];
+  const extentions = ['.com', '.es', '.io', '.net'];
+
+  let container = document.getElementById("results");
   container.innerHTML = "";
+
   let groupedResults = {};
-  dominio.forEach(ext => {
-    groupedResults[ext] = [];
+  extentions.forEach(extention => {
+    groupedResults[extention] = [];
   });
-  pronoun.forEach(el => { adj.forEach(val => { noun.forEach(item => { dominio.forEach(page => {let dominioGenerado = `${el}${val}${item}${page}`;
-                                             console.log(dominioGenerado);
-                                             groupedResults[page].push(dominioGenerado);});});});});
-    for (let key in groupedResults) {
-      let tableContainer = document.createElement("div");
-      tableContainer.classList.add("table-container");
-    let tableTitle = document.createElement("h2");
-    tableTitle.textContent = `Dominios con terminación '${key}'`;
-    tableContainer.appendChild(tableTitle);
+
+  pronouns.forEach(pronoun => {
+    adjectives.forEach(adjective => {
+      nouns.forEach(noun => {
+        extentions.forEach(extention => {
+          let generatedDomain = `${pronoun}${adjective}${noun}${extention}`;
+          groupedResults[extention].push(generatedDomain);
+        });
+      });
+    });
+  });
+
+  for (let key in groupedResults) {
+    let colDiv = document.createElement("div");
+    colDiv.className = "col-sm-6 col-md-4 col-lg-3 my-3";
+
+    let card = document.createElement("div");
+    card.className = "card shadow-sm h-100";
+
+    let cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+
+    let tableTitle = document.createElement("h4");
+    tableTitle.className = "card-title";
+    tableTitle.textContent = `Domains that end with '${key}'`;
+
     let table = document.createElement("table");
-    table.classList.add("table", "table-bordered");
+    table.className = "table table-striped table-hover table-bordered mt-3";
     table.innerHTML = `
-      <thead>
+      <thead class="thead-dark">
         <tr>
+          <th>Domain</th>
         </tr>
       </thead>
       <tbody></tbody>
     `;
-    let tbody = table.getElementsByTagName("tbody")[0];
-    let fragmento = document.createDocumentFragment();
+
+    let tbody = table.querySelector("tbody");
+
     groupedResults[key].forEach(dom => {
       let tr = document.createElement("tr");
-      let tdDominio = document.createElement("td");
-      tdDominio.textContent = dom;
-      tr.appendChild(tdDominio);
-      fragmento.appendChild(tr);
+      let tdDomain = document.createElement("td");
+      tdDomain.textContent = dom;
+      tr.appendChild(tdDomain);
+      tbody.appendChild(tr);
     });
-    tbody.appendChild(fragmento);
-    tableContainer.appendChild(table);
-    container.appendChild(tableContainer);
+    cardBody.appendChild(tableTitle);
+    cardBody.appendChild(table);
+    card.appendChild(cardBody);
+    colDiv.appendChild(card);
+    container.appendChild(colDiv);
   }
 };
